889b213f15467d296be34e3591696e90c3f80b8b
GASHIWINGO <117731094+GASHIWINGO@users.noreply.github.com>
2022-11-08 14:21:18 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,10 @@
+﻿program LR1z1;
+var x:real;
+begin
+  writeln('Введите x');
+  readln(x);
+    if x<-9 then writeln('y = ',sin(x)+x*x, ' при x = ', x)
+    else if (x>=9) and (x<1) then writeln('y = ',(ln(x)/ln(10))*cos(x)+cos(2*x)/exp(ln(x)*(1/3)), ' при x = ', x)
+    else if (x>=1) and (x<5) then writeln('y = ',-x*(ln(x)/ln(10)),' при x = ', x)
+    else if x>=5 then writeln('y = ',sin(x)/exp(ln(x)*(0.1*x)-(sin(x)/cos(x))/cos(2*x)),' при x = ', x);
+end.
---
+++ 2.pas
@@ -0,0 +1,16 @@
+﻿program LR1z2;
+const a=-11;b=7;h=0.3;
+var x:real;
+begin
+  x:=a;
+  while x<=b do 
+  begin
+    
+    if x<-9 then writeln('y = ',sin(x)+x*x, ' при x = ', x)
+    else if (x>=9) and (x<1) then writeln('y = ',(ln(x)/ln(10))*cos(x)+cos(2*x)/exp(ln(x)*(1/3)), ' при x = ', x)
+    else if (x>=1) and (x<5) then writeln('y = ',-x*(ln(x)/ln(10)),' при x = ', x)
+    else if x>=5 then writeln('y = ',sin(x)/exp(ln(x)*(0.1*x)-(sin(x)/cos(x))/cos(2*x)),' при x = ', x);
+    x:=x+h
+  end;
+end.
+      
